525e7e962dc0d78d36e8bb2732ff2298795b9ce8
tanakorn0314 <tanakorn0412@gmail.com>
2019-02-13 18:38:11 +0700

    Updates

---
+++ precache-manifest.bb2dd22243377114beecf6996006b69d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa03d063b3c5d6b4a752",
+    "url": "/React_Redux_Apori_DM/static/js/runtime~main.fa03d063.js"
+  },
+  {
+    "revision": "df0a57211f37672e3556",
+    "url": "/React_Redux_Apori_DM/static/js/main.df0a5721.chunk.js"
+  },
+  {
+    "revision": "df1bb44f906228ef3963",
+    "url": "/React_Redux_Apori_DM/static/js/2.df1bb44f.chunk.js"
+  },
+  {
+    "revision": "df0a57211f37672e3556",
+    "url": "/React_Redux_Apori_DM/static/css/main.125b16a9.chunk.css"
+  },
+  {
+    "revision": "da13a5aaea2a81d8ca2fc893e0e45843",
+    "url": "/React_Redux_Apori_DM/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/apiori-algorithm/precache-manifest.22bc3844cde865673d009b3dfa17004f.js"
+  "/React_Redux_Apori_DM/precache-manifest.bb2dd22243377114beecf6996006b69d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/apiori-algorithm/index.html", {
+workbox.routing.registerNavigationRoute("/React_Redux_Apori_DM/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
